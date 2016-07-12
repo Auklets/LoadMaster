f1e60e9cba2b0a28784dc7e2c4a07858bb34b5d7
cshg <csh@christianhaug.com>
2016-07-12 14:35:49 -0700

    Add mysql link to prod master

--- helper/utils.js
+++ helper/utils.js
@@ -10,7 +10,7 @@ const createContainer = (dockerConnection, masterName, imageName, containerName)
       name: containerName,
       HostConfig: {
         Binds: ["/env:/env"],
-        Links: [masterName + ":masterhost"],
+        Links: ["mysql:mysql", masterName + ":masterhost"],
       },
     },
     (connectErr, container) => {
